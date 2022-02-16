3efc77098e01d466f9c589e9d1a6a725b675a0fc
MHannan <mdhannan.info@gmail.com>
2022-02-16 12:50:58 +0600

    Product showing not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyle';
 import Hero from './Partials/Hero';
+import Products from './Partials/Products';
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products />
     </Router>
   );
 }
